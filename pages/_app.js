53f5a3d5ce3abd1c9de3afe33a51dc4d2b250e44
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-02-19 03:17:35 +0530

    Firebase and Github Auth Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { AuthProvider } from '@/lib/auth';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
