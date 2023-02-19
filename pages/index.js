272e91f215e90cb8954083babc5d4fd23a33955c
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-19 13:19:29 +0530

    Save User to Firestore

--- pages/index.js
+++ pages/index.js
@@ -15,37 +15,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
         <div className={styles.center}>
           <h1>Fast Feedback</h1>
         </div>
 
-        {!user && <button onClick={() => signinWithGitHub()}>Sign In</button>}
-        {user?.displayName}
-        {user && <button onClick={() => signout()}>Sign Out</button>}
+        {user?.name}
+        {user ? (
+          <button onClick={() => signout()}>Sign Out</button>
+        ) : (
+          <button onClick={() => signinWithGitHub()}>Sign In</button>
+        )}
       </main>
     </>
   );
