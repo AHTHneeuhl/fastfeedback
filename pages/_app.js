c6b8428d87ecf7329f14e1d62391d98632380f4e
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-02-19 15:00:20 +0530

    Chakra-UI and Font Setup (#4)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,47 @@
+import Head from 'next/head';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
 import { AuthProvider } from '@/lib/auth';
-import '@/styles/globals.css';
+import { Inter } from '@next/font/google';
+import customTheme from '@/styles/theme';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <main className={inter.className}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </main>
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
