0a12f5feb9bf387ab3a4f4a29d404bca937cf5ee
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-19 23:19:00 +0530

    Firebase Admin with Next.js + SWR

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -11,6 +11,7 @@ import {
   Button,
   Box,
 } from '@chakra-ui/react';
+import AddSiteModal from './AddSiteModal';
 
 const DashboardShell = ({ children }) => {
   const { user, signout } = useAuth();
@@ -39,10 +40,12 @@ const DashboardShell = ({ children }) => {
             <Link>Feedback</Link>
           </Flex>
           <Flex justifyContent="center" alignItems="center">
-            <Button variant="ghost" mr={2} onClick={() => signout()}>
-              Log Out
-            </Button>
-            <Avatar size="sm" src={user.photoUrl} />
+            {user && (
+              <Button variant="ghost" mr={2} onClick={() => signout()}>
+                Log Out
+              </Button>
+            )}
+            <Avatar size="sm" src={user?.photoUrl} />
           </Flex>
         </Flex>
       </Flex>
@@ -54,18 +57,7 @@ const DashboardShell = ({ children }) => {
         </Breadcrumb>
         <Flex justifyContent="space-between">
           <Heading mb={8}>My Sites</Heading>
-          <Button
-            backgroundColor="gray.900"
-            color="white"
-            fontWeight="medium"
-            _hover={{ bg: 'gray.700' }}
-            _active={{
-              bg: 'gray.800',
-              transform: 'scale(0.95)',
-            }}
-          >
-            + Add Site
-          </Button>
+          <AddSiteModal>+ Add Site</AddSiteModal>
         </Flex>
         {children}
       </Flex>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,32 @@
 import { useAuth } from '@/lib/auth';
+import Head from 'next/head';
+import useSWR from 'swr';
+
 import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboardShell';
+import fetcher from '@/utils/fetcher';
+import SiteTable from '@/components/SiteTable';
 
 const Dashboard = () => {
-  const { user } = useAuth();
+  const { data: sites, isLoading } = useSWR('/api/sites', fetcher);
 
-  if (!user) {
-    return 'Loading...';
-  }
-
-  return <EmptyState />;
+  return (
+    <>
+      <Head>
+        <title>Fast Feedback | Dashboard</title>
+      </Head>
+      <DashboardShell>
+        {isLoading ? (
+          <SiteTableSkeleton />
+        ) : sites ? (
+          <SiteTable sites={sites} />
+        ) : (
+          <EmptyState />
+        )}
+      </DashboardShell>
+    </>
+  );
 };
 
 export default Dashboard;
