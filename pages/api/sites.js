3386045897f656e9493bb49d8118c2af50c8ddab
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-24 23:47:57 +0530

    Squashing bugs, integration tests, and logging

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,12 +1,28 @@
 import { getUserSites } from '@/lib/db-admin';
 import { auth } from '@/lib/firebase-admin';
+import logger, { formatObjectKeys } from '@/utils/logger';
 
 export default async function handler(req, res) {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
     const { sites } = await getUserSites(uid);
+
     res.status(200).json(sites);
   } catch (error) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method,
+        },
+        response: {
+          statusCode: res.statusCode,
+        },
+      },
+      error.message
+    );
+
     res.status(500).json({ error });
   }
 }
